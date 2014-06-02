8d3c7e6e58d0c01f4bdf0e31081ec39b3fb19a0d
Maimer <nesnl23@yahoo.com>
2014-06-02 11:18:00 -0400

    Added down payment and total interest methods

--- mortgage.rb
+++ mortgage.rb
@@ -1,26 +1,30 @@
 class Mortgage
   attr_reader :home_value, :apr, :duration_in_years
 
-  def initialize(home_value, apr, duration)
+  def initialize(home_value, down_payment_percentage, apr, duration)
     @home_value = home_value
+    @down_payment_percentage = down_payment_percentage
     @apr = apr
     @duration_in_years = duration
   end
 
   def down_payment
-
+    @down_payment_percentage * @home_value
   end
 
   def monthly_payment
-    ((@home_value * @apr * ((1 + @apr)**(@duration_in_years * 12))) /
+    (((@home_value - down_payment) * @apr *
+    ((1 + @apr)**(@duration_in_years * 12))) /
     ((1 + @apr)**(@duration_in_years * 12) - 1))
   end
 
   def total_interest_paid
-
+    ((monthly_payment * (@duration_in_years * 12)) -
+    (@home_value - @down_payment)
   end
 end
 
-a = Mortgage.new(10000, 0.01, 20)
+a = Mortgage.new(100000, 0.20, 0.01, 20)
 
 puts a.monthly_payment
+puts a.down_payment
